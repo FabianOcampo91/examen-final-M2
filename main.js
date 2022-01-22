20521da098be22d65c50b1e6fd8d6e5da4dcd6f3
FabianOcampo91 <faby.ocampo91@gmail.com>
2022-01-21 21:08:03 -0300

    EXAMEN FINAL M2

--- main.js
+++ main.js
@@ -5,74 +5,62 @@ const container = document.getElementById('container');
 const users = 'https://tiendavirtualmern.herokuapp.com/api/users';
 const login = 'https://tiendavirtualmern.herokuapp.com/api/users/login';
 
+const signUpxButton = document.getElementById("signUpx");
+const signInxButton = document.getElementById("signInx");
 
 
-signUpButton.addEventListener('click', () => {
-  container.classList.add('right-panel-active');
-
-  function usuario (nombre, email, password) {
-    this.nombre = nombre,
-    this.email= email,
-    this.password = password
-  }
-
-  let nameCapture = document.getElementById("name").value;
-  let emailCapture = document.getElementById("email").value;
-  let passCapture = document.getElementById("pass").value;
-  
-  const nuevoUsuario = new usuario(nameCapture, emailCapture, passCapture)
-
-  fetch(users, {
-    method: 'POST',
-    body: JSON.stringify(nuevoUsuario),
-    
-  })
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(console.error())
-
+signUpButton.addEventListener('click', (e) => {
+  container.classList.add('right-panel-active'); 
 });
-
 
 signInButton.addEventListener('click', () => {
   container.classList.remove('right-panel-active');
 });
 
-
-// const nombre = document.querySelector('#name');
-// console.log(signUpButton);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// signUpButton.addEventListener('click', () => {
-//   const nuevoUser = {
-//     name: '',
-//     email: '',
-//     password: '',
-//   };
+signUpxButton.addEventListener('click', function registrar () {
   
+  function usuario (nombre, email, password) {
+    this.nombre = nombre,
+    this.email= email,
+    this.password = password
+  }
+  
+  let nameCapture = document.getElementById("name").value;
+  let emailCapture = document.getElementById("email").value;
+  let passCapture = document.getElementById("pass").value;
+  
+  const nuevoUsuario = new usuario(nameCapture, emailCapture, passCapture);
+  
+  fetch(users, {
+    method: 'POST',
+    body: JSON.stringify(nuevoUsuario),
+    headers: {
+      "Content-type": "aplication/json"
+    }
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
 
-// });
+});
 
+signInxButton.addEventListener('click', function logeo(){
+  function logear (email, password){
+    this.email = email,
+    this.password = password
+  }
+
+  const logearEmail = document.getElementById('#e-mail');
+  const logearPass = document.getElementById('#password');
+
+  const usuarioLogeado = new logear (logearEmail, logearPass);
+
+  fetch(login, {
+    method: 'POST',
+    body: JSON.stringify(usuarioLogeado),
+    headers: {
+      "Constent-type": "aplicatio/json"
+    }
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+})
